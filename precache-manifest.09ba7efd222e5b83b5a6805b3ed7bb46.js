dec0a6ae53fd13bd584cad0408c89840c63ddb6d
SasungKim <freeboy81520@gmail.com>
2020-06-13 07:53:04 -0400

    Updates

---
+++ precache-manifest.09ba7efd222e5b83b5a6805b3ed7bb46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d61ac8d4451146b7d1061c397c3668",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "5ec5493abb052e793616",
+    "url": "/movie_app/static/css/main.8a5c2427.chunk.css"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ec5493abb052e793616",
+    "url": "/movie_app/static/js/main.87efb9e8.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
